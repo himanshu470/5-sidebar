2b5be99ee5600d9bf02ea69022a52a5e0f9fb106
himanshu <rawathimanshu3928@gmail.com>
2021-03-13 12:39:14 +0530

    this is 5 project

---
+++ JS/app.js
@@ -0,0 +1,22 @@
+const togglebtn = document.querySelector(".sidebar-toggle");
+const closebtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+togglebtn.addEventListener("click", function () {
+    console.log(sidebar.classList);
+    // if (sidebar.classList.contains("show-sidebar")){
+    //     sidebar.classList.remove("show-sidebar")
+    // } else {
+    //     sidebar.classList.add("show-sidebar");
+    // }
+
+//in one line
+
+    sidebar.classList.toggle("show-sidebar");
+
+});
+
+closebtn.addEventListener('click', function () {
+    sidebar.classList.remove("show-sidebar");
+})
+
